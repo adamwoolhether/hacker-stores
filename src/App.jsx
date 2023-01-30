28304c6417750942cc390764fbee999d74fc9c67
adamwoolhether <adamwoolhether@gmail.com>
2023-01-31 06:43:39 +0800

    Secondary(child) React components

--- src/App.jsx
+++ src/App.jsx
@@ -43,24 +43,43 @@ function App() {
         <div>
             <h1>{welcome.greeting} {getTitle(title)}</h1>
 
+            <Search />
+
+            <hr />
+
+            <List />
+        </div>
+    )
+}
+
+function Search() {
+    return (
+        <div>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
+        </div>
+    )
+}
 
+// List demonstrates the use of a secondary React component
+function List() {
+    return (
+        <div>
             <ul>
                 {/* Note the requirement for a key in a <li>
                 which allows React to efficiently update the list if needed.
                 You can use the index if no key is given, but this should be avoided if possible.*/}
                 {list.map(function(item){
-                return (
-                    <li key={item.objectID}>
+                    return (
+                        <li key={item.objectID}>
                         <span>
                             <a href={item.url}>{item.title}</a>
                         </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                    </li>
-                );
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </li>
+                    );
                 })}
             </ul>
         </div>
