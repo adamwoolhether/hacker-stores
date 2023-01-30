9082139d8c156863ba7f60e346ddb60d32780f32
adamwoolhether <adamwoolhether@gmail.com>
2023-01-31 06:23:50 +0800

    LISTS

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,28 @@ const welcome = {
     title: title,
 }
 
+// Data in JS often comes as an array.
+// We use the arrays' built in map() method to do so.
+// See below `list.map` for use.
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    }
+];
+
 function getTitle(title) {
     return title
 }
@@ -23,6 +45,15 @@ function App() {
 
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" />
+
+            <ul>
+                {/* Note the requirement for a key in a <li>
+                which allows React to efficiently update the list if needed.
+                You can use the index if no key is given, but this should be avoided if possible.*/}
+                {list.map(function(item){
+                return <li key={item.objectID}>{item.title}</li>;
+                })}
+            </ul>
         </div>
     );
 }
