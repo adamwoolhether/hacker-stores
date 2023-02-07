14ed94d5582c1c62e59fcc6f958ac3ef9153484d
adamwoolhether <adamwoolhether@gmail.com>
2023-02-07 23:06:18 +0800

    cleaned up comments, preparing for further branches of study

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,11 @@ import axios from 'axios';
 // Keeping with hook naming convention, is uses 'use' in front of the name, and
 // return values are returned as an array.
 const useStorageState = (key, initialState) => {
-    // React's `useState` Hook is how we define a stateful value. It returns an array with
-    // two entries: `searchTerm` represents the current state, & `setSearchTerm`
-    // is a function to update the state, AKA `state updater function`.
-    // `useState` is an example of a 'React hook', one of many.
-    // If a component below needs to update State, pass a callback handler.
-    // If a component below needs to use state, pass it down as props.
-    // Use cached search term if it exists, or default to 'React'.
     const [value, setValue] = React.useState(
         localStorage.getItem(key) || initialState
     );
 
-    // React's `useEffect` Hook is used to trigger side-effect.
-    // 'useEffect's first arg is a func to run our side effect
-    // and store `searchTerm in browser's local storage. Second
-    // arg is a dependency array of vars.
     React.useEffect(() => {
-        // The 'side-effect' is handled in a centralized place, not in a specific function,
-        // to keep the local storage updated.
         localStorage.setItem(key, value);
     }, [value])
 
@@ -30,55 +17,6 @@ const useStorageState = (key, initialState) => {
 }
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
-
-// We've removed the `initialStories` object and `getAsyncStories`
-// funcs in favor of fetching real API data.
-/*// Data in JS often comes as an array.
-// We use the arrays' built in map() method to do so.
-// See below `list.map` for use.
-const initialStories = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    }
-];
-
-// getAsyncStories returns a promise that returns data in its shorthand version.
-/!*const getAsyncStories = () =>
-    Promise.resolve({ data: { stories: initialStories } });*!/
-// Non-shorthand for reference.
-/!*const getAsyncStories = () =>
-    new Promise((resolve) =>
-        resolve({ data: { stories: initialStories } })
-    );*!/
-// We can simulate a remote API call by removing the shorthand version,
-// and delay the promise's resolve:
-const getAsyncStories = () =>
-    new Promise((resolve) =>
-        setTimeout(
-            () => resolve({ data: { stories: initialStories } }),
-            2000
-        )
-    );
-// Example of an 'impossible state' bug. We should be cautious using multiple
-// state updater functions in a row. NOTE: the bug is fixed now, as we've put
-// the multiple state hook's into a single useReducer.
-/!*const getAsyncStories = () =>
-    new Promise((resolve, reject) => setTimeout(reject, 2000));*!/*/
-
-
 
 // storiesReducer is a 'reducer function'. Reducer functions always take
 // a `state` and `action`. The reducer will always return a new state
@@ -136,21 +74,11 @@ const App = () => {
         `${API_ENDPOINT}${searchTerm}`
     );
 
-    /*// Make our list stateful with the useState Hook, setting initial state
-    // as an empty array, so we can simulate fetching the data asynchronously.
-    const [stories, setStories] = React.useState([]);*/
-    // useReducer Hook sets state implicitly by dispatching an item for the reducer.
-    // useReducer is preferred over userState when we have multiple values that
-    // are dependent on each other or related to one domain.
     const [stories, dispatchStories] = React.useReducer(
         storiesReducer,
         { data: [], isLoading: false, isError: false }
     );
 
-    // React's useCallback createsa memoized function every time its dependency
-    // array (E) changes. This causes the useEffect hook to run again (C) because
-    // it depends on the new function (D), so we run the fetch again when `searchTerm`
-    // changes because the useEffect depends on `handleFetchStories`.
     const handleFetchStories = React.useCallback(async() => { // use async
         dispatchStories({type: 'STORIES_FETCH_INIT'});
 
@@ -165,44 +93,7 @@ const App = () => {
         }
     }, [url]);
 
-        // React's native 'Fetch API'
-        /*        fetch(url)
-                    .then((response) => response.json()) // translate the response into json.
-                    .then((result) => {
-                        dispatchStories({
-                            type: 'STORIES_FETCH_SUCCESS',
-                            payload: result.hits, // use the returned data structure's key 'hits'.
-                        });
-                    })
-                    .catch(() =>
-                        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-                    );
-            }, [url]); // E*/
-        // Using axios
- /*       axios
-            .get(url)
-            .then((result) => {
-                dispatchStories({
-                    type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.data.hits,
-                });
-            })
-            .catch(() =>
-                dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-            );
-    }, [url]);*/
 
-    /*// We've taken the below hooks and merged them into the 'useReducer' hook above.
-    // This helps reduce the chance bugs, giving a unified state management.
-    // implement conditional rendering for user feedback while the list retrieval is loading.
-    const [isLoading, setIsLoading] = React.useState(false);
-    // Enable handling of potential errors if occurred while fetching remote data.
-    // Actual handling is done with the promise's 'catch()' block in 'userEffect' below.
-    const [isError, setIsError] = React.useState(false);*/
-
-    // 'useEffect' hook to call the function and resolve the returned promise
-    // as a side effect. We give and empty deps array so the side-effect only
-    // runs once the component renders for the first time.
     React.useEffect(() => {
         handleFetchStories(); // C
     }, [handleFetchStories]); // D
@@ -232,10 +123,6 @@ const App = () => {
     const searchedStories = stories.data.filter((story) =>
          story.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
-    /*A more verbose way to write the same func:
-    const searchedStories = stories.filter(function (story) {
-        return story.title.includes(searchTerm)
-    });*/
 
     return (
         <div>
@@ -283,37 +170,19 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
 )
 
 const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
-    /*// Destructuring the prop object, allowing easy access.
-    // Another way(above) is to destructure is directly in the functions signature
-    const { search, onSearch } = props;*/
-
-    // A
-    // Create a 'ref' with 'useRef' hook. This ref is a persistent value
-    // which stays intact for the lifetime of a React component. It's `current`
-    // property is mutable, unlike the actual ref object.
     const inputRef = React.useRef();
 
-    // C
-    // Opt into the React lifecycle with the 'useEffect' hook.
     React.useEffect(() => {
         if (isFocused && inputRef.current) {
-        // D
-            // The ref is passed the element's `ref` attribute, allowing the
-            // `current` property to access the element. We execute it's functionality
-            // as a side effect if `isFocused` is set and the `current` property exists.
+
         inputRef.current.focus()
     }
 }, [isFocused]);
 
     return(
-//  React Fragments allow returning siblings elements side by side without a top-level element.
-//  Can be called with `<React.Fragment>`  `</React.Fragment>` or shorthand: `<>` `</>`
         <>
             <label htmlFor={id}>{children}</label>
             &nbsp;
-            {/* B
-            The ref is passed to the element's JSX-reserved `ref` attribute, assigning
-            the element is assigned to the `current` property.*/}
             <input
                 ref={inputRef}
                 id={id}
@@ -329,9 +198,6 @@ const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, ch
 // List demonstrates the use of a secondary React component.
 const List = ({list, onRemoveItem}) => (
     <ul>
-        {/* Note the requirement for a key in a <li>
-        which allows React to efficiently update the list if needed.
-        You can use the index if no key is given, but this should be avoided if possible.*/}
         {list.map((item) => (
         <Item
             key={item.objectID}
@@ -341,49 +207,8 @@ const List = ({list, onRemoveItem}) => (
             ))}
     </ul>
 );
-// A variation of List using spread and rest operators.
-/*const List = ({ list }) => (
-    <ul>
-        // rest operator destructures the `objectID` from the rest of item object.
-        {list.map(({ objectID, ...item}) => (
-            // spread operator spreads `item` with its key/value pairs
-            <Item key={item.objectID} {...item} />
-            /* // We can clean all this up with the spread operator above
-              <Item
-                key={item.objectID}
-                title={item.title}
-                url={item.url}
-                author={item.author}
-                num_comments={item.num_comments}
-                points={item.points}
-            />
-        ))}
-    </ul>
-);
 
-// Variation of Item using spread and rest operators.
-// Notice that although the function signature is more
-// concise than variation 2, we now messy of the List func.
-const Item = ({ title, url, author, num_comments, points }) => (
-    <li>
-        <span>
-            <a href={url}>{title}</a>
-        </span>
-        <span>{author}</span>
-        <span>{num_comments}</span>
-        <span>{points}</span>
-    </li>
-)*/
 const Item = ({ item, onRemoveItem }) => {
-    // We don't use the handleRemoveItem, & instead favor the use of
-    // an 'inline handler', which allows us to execute the callback
-    // function directly in the JSX. There are two method (see below button).
-    // NOTE: Inline handlers may be more concise, but they can obscure
-    // functionality and harm readability. Our use here is ok though.
-    // const handleRemoveItem = () => {
-    //     onRemoveItem(item);
-    // };
-
     return (
         <li>
             <span>
@@ -393,13 +218,6 @@ const Item = ({ item, onRemoveItem }) => {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-                {/*
-                // original way, using func above.
-                <button type="button" onClick={handleRemoveItem}>
-                // using jS 'bind' method
-                <button type="button" onClick={onRemoveItem.bind(null, item)}>
-                // The most popular method, using and inline arrow function:
-                */}
                 <button type="button" onClick={() => onRemoveItem(item)}>
                     Dismiss
                 </button>
@@ -407,26 +225,4 @@ const Item = ({ item, onRemoveItem }) => {
         </li>
     );
 };
-/*// An example of the Item component using `nested destructuring`.
-// It helps to quickly identify all of `item`'s info, but as you
-// can see it also adds a lot of clutter and awkward indentation.
-// It may be useful/clearer in other scenarios though.
-const Item = ({
-    item: {
-        title,
-        url,
-        author,
-        num_comments,
-        points,
-    },
-}) => (
-    <li>
-        <span>
-            <a href={url}>{title}</a>
-        </span>
-        <span>{author}</span>
-        <span>{num_comments}</span>
-        <span>{points}</span>
-    </li>
-)*/
 export default App
