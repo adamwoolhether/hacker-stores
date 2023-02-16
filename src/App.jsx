a4bd56ae427a3bc2c11e2ac0388ae85db9a076bd
adamwoolhether <adamwoolhether@gmail.com>
2023-02-16 21:03:19 +0800

    CSS-in-JS

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,90 @@ import axios from 'axios';
 import styles from './App.module.css';
 // allows us to use string interpolation to use two styles in one element on the SearchFrom components
 import clsx from 'clsx';
+import styled from 'styled-components';
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// We can also use a CSS-in-JS approach:
+const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+  color: #171212;
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  a {
+    color: inherit;
+  }
+
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  display: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // useStorageState is a custom React Hook. It wraps `useState` and `useEffect`.
 // Keeping with hook naming convention, is uses 'use' in front of the name, and
@@ -129,12 +213,8 @@ const App = () => {
     );
 
     return (
-        // Style the attribute with css. We remove the `<hr />` because CSS handles the border.
-        // The commented out code shows how to use css without css modules enabled.
-       /*<div className="container">
-           <h1 className="headline-primary">My Hacker Stories</h1>*/
-        <div className={styles.container}>
-            <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
+        <StyledContainer>
+            <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 
             <SearchForm
                 searchTerm={searchTerm}
@@ -142,22 +222,38 @@ const App = () => {
                 onSearchSubmit={handleSearchSubmit}
             />
 
-            {stories.isError && <p>Something went wrong...</p>}
+            <hr />
 
-            {/*Using a 'ternary operator' to determine whether feedback is rendered or not.*/}
+            {stories.isError && <p>Something went wrong ...</p>}
+
             {stories.isLoading ? (
-                <p>Loading...</p>
+                <p>Loading ...</p>
             ) : (
-                <List
-                    list={searchedStories}
-                    onRemoveItem={handleRemoveStory}
-                />
+                <List list={stories.data} onRemoveItem={handleRemoveStory} />
             )}
-        </div>
+        </StyledContainer>
     );
 };
 
-const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
+const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
+    <StyledSearchForm onSubmit={onSearchSubmit}>
+        <InputWithLabel
+            id="search"
+            value={searchTerm}
+            isFocused
+            onInputChange={onSearchInput}
+        >
+            <strong>Search:</strong>
+        </InputWithLabel>
+
+        <StyledButtonLarge type="submit" disabled={!searchTerm}>
+            Submit
+        </StyledButtonLarge>
+    </StyledSearchForm>
+);
+
+// Using CSS-in-CSS
+/*const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     // <form onSubmit={onSearchSubmit} className="search-form"> // without css modules
     <form onSubmit={onSearchSubmit} className={styles.searchForm}>
         <InputWithLabel
@@ -178,34 +274,27 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
             Submit
         </button>
     </form>
-)
+)*/
 
 const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
     const inputRef = React.useRef();
 
     React.useEffect(() => {
         if (isFocused && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
 
-        inputRef.current.focus()
-    }
-}, [isFocused]);
-
-    return(
+    return (
         <>
-            {/*// className can also be passed as a prop to React components.*/}
-            {/*<label htmlFor={id} className="label">*/}
-            <label htmlFor={id} className={styles.label}>
-                {children}
-            </label>
+            <StyledLabel htmlFor={id}>{children}</StyledLabel>
             &nbsp;
-            <input
+            <StyledInput
                 ref={inputRef}
                 id={id}
                 type={type}
                 value={value}
-                autoFocus={isFocused}
                 onChange={onInputChange}
-                className={styles.input}
             />
         </>
     );
@@ -225,6 +314,26 @@ const List = ({list, onRemoveItem}) => (
 );
 
 const Item = ({ item, onRemoveItem }) => (
+    <StyledItem>
+        <StyledColumn width="40%">
+            <a href={item.url}>{item.title}</a>
+        </StyledColumn>
+        <StyledColumn width="30%">{item.author}</StyledColumn>
+        <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+        <StyledColumn width="10%">{item.points}</StyledColumn>
+        <StyledColumn width="10%">
+            <StyledButtonSmall
+                type="button"
+                onClick={() => onRemoveItem(item)}
+            >
+                Dismiss
+            </StyledButtonSmall>
+        </StyledColumn>
+    </StyledItem>
+);
+
+// Using CSS-in-CSS
+/*const ItemCSSinCSS = ({ item, onRemoveItem }) => (
     // JSX can be passed as an inline JS object to attributes, demonstrated in the below `style` attributes.
     // Inline style is good for prototyping and dynamic style defs, but should be used sparingly,
     // it's better to keep a separate style definition in a CSS file.
@@ -247,5 +356,5 @@ const Item = ({ item, onRemoveItem }) => (
              </button>
         </span>
     </li>
-);
+);*/
 export default App
